9ea1e607f67facfe124715f30c413421f02733d3
3343780376 <3343780376@qq.com>
2021-04-15 17:32:36 +0800

    add README.md

--- handles.go
+++ handles.go
@@ -4,5 +4,5 @@ func UseEchoHandle() {
 
 	AddCommandHandle(func(event Event, bot *Bot, args []string) {
 		bot.SendMsg(event.MessageType, event.UserId, event.GroupId, args[0], false)
-	}, ".echo", nil, nil, 10, false)
+	}, "/echo", nil, nil, 10, false)
 }
